8f3c8747b579daaf5d4e80e7542abb8c56bfd320
PatriciaDivi <patriciavianad@gmail.com>
2022-02-21 20:55:40 -0300

    7 Requisito | Crie um componente chamado Missions | :rocket:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
-// import Title from './components/Title';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
+
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Header />
         <main>
           <SolarSystem />
+          <Missions />
         </main>
       </>
     );
---
+++ src/components/Missions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Mission extends React.Component {
+    render() {
+        return (
+            <div data-testid="missions">
+                <p>Mission</p>
+            </div>
+        )
+    }
+}
+
+export default Mission;
